854b304080ca8bcc7874291cf26a3d6cbc656c33
nozingu7 <39980141+nozingu7@users.noreply.github.com>
2018-11-10 11:19:16 +0900

    NGUI INVENTORY
    ...

---
+++ SoundAction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundAction : MonoBehaviour
+{
+    public static SoundAction instance = null;
+    public AudioClip[] clips;
+    public AudioSource audio;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            return;
+        }
+    }
+    // Use this for initialization
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            audio.volume = 0.5f;
+            audio.PlayOneShot(clips[0]);
+        }
+
+    }
+}
---
+++ TweenOption.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenOption : MonoBehaviour {
+
+    TweenScale ts;
+    TweenPosition tp;
+    public GameObject message;
+
+    private void Start()
+    {
+        ts = GetComponent<TweenScale>();
+        tp = GetComponent<TweenPosition>();
+    }
+
+    public void CencleFunc()
+    {
+        if(message.activeSelf)
+        {
+            message.SetActive(false);
+
+        }
+    }
+}
